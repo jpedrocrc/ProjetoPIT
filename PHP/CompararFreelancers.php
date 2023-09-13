847383d0f4f055c0cd6f556e5655138319a459f3
João Pedro Cerqueira Rodrigues <jpedrocrc@hotmail.com>
2023-09-13 17:27:41 -0300

    Correção erro form

--- PHP/CompararFreelancers.php
+++ PHP/CompararFreelancers.php
@@ -42,76 +42,76 @@
 </head>
 
 <body class="bg-image" style="background-image: url('../PHP/mainimg.png');">
-<form class=" bg-dark text-white position-absolute top-50 start-50 translate-middle">
-<?php
-ini_set('default_charset', 'UTF-8');
-include_once './Classes/config.php';
-require_once './Classes/ConexaoBD.php';
-require_once './Classes/CompararFreelancers.php';
+<div class="bg-dark text-white position-absolute top-50 start-50 translate-middle">
+    <?php
+    ini_set('default_charset', 'UTF-8');
+    include_once './Classes/config.php';
+    require_once './Classes/ConexaoBD.php';
+    require_once './Classes/CompararFreelancers.php';
 
-$conexao = new ConexaoBD($nomeServidor, $nomeUsuario, $senha, $nomeBanco);
-$conexao->conectar();
+    $conexao = new ConexaoBD($nomeServidor, $nomeUsuario, $senha, $nomeBanco);
+    $conexao->conectar();
 
-$listaFreelancers = new CompararFreelancers($conexao);
-$freelancers = $listaFreelancers->GetFreelancers();
+    $listaFreelancers = new CompararFreelancers($conexao);
+    $freelancers = $listaFreelancers->GetFreelancers();
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['freelancer1']) && isset($_POST['freelancer2'])) {
-    $freelancer1Id = $_POST['freelancer1'];
-    $freelancer2Id = $_POST['freelancer2'];
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['freelancer1']) && isset($_POST['freelancer2'])) {
+        $freelancer1Id = $_POST['freelancer1'];
+        $freelancer2Id = $_POST['freelancer2'];
 
-    $freelancer1Dados = $listaFreelancers->GetFreelancerPorId($freelancer1Id);
-    $freelancer2Dados = $listaFreelancers->GetFreelancerPorId($freelancer2Id);
+        $freelancer1Dados = $listaFreelancers->GetFreelancerPorId($freelancer1Id);
+        $freelancer2Dados = $listaFreelancers->GetFreelancerPorId($freelancer2Id);
 
-    if ($freelancer1Dados !== null && $freelancer2Dados !== null) {
-        echo "<h3 class='m-2'>Comparação de Freelancers</h3>";
-        echo "<div class='container'>";
-        
-        echo "<div class='table-container'>";
-        echo "<table>";
-        echo "<tr><th>Freelancer 1</th></tr>";
-        echo "<tr><td>";
-        $listaFreelancers->MostrarFreelancersSelecionados($freelancer1Dados);
-        echo "</td></tr>";
-        echo "</table>";
-        echo "</div>";
-        
-        echo "<div class='table-container'>";
-        echo "<table>";
-        echo "<tr><th>Freelancer 2</th></tr>";
-        echo "<tr><td>";
-        $listaFreelancers->MostrarFreelancersSelecionados($freelancer2Dados);
-        echo "</td></tr>";
-        echo "</table>";
-        echo "</div>";
-        
-        echo "</div>";
+        if ($freelancer1Dados !== null && $freelancer2Dados !== null) {
+            echo "<h3 class='m-2'>Comparação de Freelancers</h3>";
+            echo "<div class='container'>";
+
+            echo "<div class='table-container'>";
+            echo "<table>";
+            echo "<tr><th>Freelancer 1</th></tr>";
+            echo "<tr><td>";
+            $listaFreelancers->MostrarFreelancersSelecionados($freelancer1Dados);
+            echo "</td></tr>";
+            echo "</table>";
+            echo "</div>";
+
+            echo "<div class='table-container'>";
+            echo "<table>";
+            echo "<tr><th>Freelancer 2</th></tr>";
+            echo "<tr><td>";
+            $listaFreelancers->MostrarFreelancersSelecionados($freelancer2Dados);
+            echo "</td></tr>";
+            echo "</table>";
+            echo "</div>";
+
+            echo "</div>";
+        } else {
+            echo "Erro ao obter dados dos freelancers selecionados.";
+        }
     } else {
-        echo "Erro ao obter dados dos freelancers selecionados.";
-    }
-} else {
-    echo "<h3 class='m-2'>Selecione dois freelancers para comparar:</h3>";
-    echo "<form action='' method='post'>";
-    echo "<div class='text-center'>";
-    echo "<label for='freelancer1'>Freelancer 1:</label>";
-    echo "<select name='freelancer1' id='freelancer1'>";
-    foreach ($freelancers as $freelancer) {
-        echo "<option value='{$freelancer['ID_FREELANCER']}'>{$freelancer['NOME']}</option>";
-    }
-    echo "</select>";
+        echo "<h3 class='m-2'>Selecione dois freelancers para comparar:</h3>";
+        echo "<form action='' method='post'>";
+        echo "<div class='text-center'>";
+        echo "<label for='freelancer1'>Freelancer 1:</label>";
+        echo "<select name='freelancer1' id='freelancer1'>";
+        foreach ($freelancers as $freelancer) {
+            echo "<option value='{$freelancer['ID_FREELANCER']}'>{$freelancer['NOME']}</option>";
+        }
+        echo "</select>";
 
-    echo "<label for='freelancer2'>Freelancer 2:</label>";
-    echo "<select name='freelancer2' id='freelancer2'>";
-    echo "</div>";
-    foreach ($freelancers as $freelancer) {
-        echo "<option value='{$freelancer['ID_FREELANCER']}'>{$freelancer['NOME']}</option>";
-    }
-    echo "</select>";
+        echo "<label for='freelancer2'>Freelancer 2:</label>";
+        echo "<select name='freelancer2' id='freelancer2'>";
+        foreach ($freelancers as $freelancer) {
+            echo "<option value='{$freelancer['ID_FREELANCER']}'>{$freelancer['NOME']}</option>";
+        }
+        echo "</select>";
 
-    echo "<div class='mt-2 text-center'><button class='btn btn-light' type='submit'>Comparar</button></div>";
-    echo "</form>";
-}
-?>
-    </div>
+        echo "<div class='mt-2 text-center'><button class='btn btn-light' type='submit'>Comparar</button></div>";
+        echo "</div>";
+        echo "</form>";
+    }
+    ?>
+</div>
 </body>
 
 </html>
--- PHP/CompararServiços.php
+++ PHP/CompararServiços.php
@@ -24,75 +24,81 @@
 </head>
 
 <body class="bg-image" style="background-image: url('../PHP/mainimg.png');">
-<form class=" bg-dark text-white position-absolute top-50 start-50 translate-middle">
-<?php
-ini_set('default_charset', 'UTF-8');
-include_once './Classes/config.php';
-require_once './Classes/ConexaoBD.php';
-require_once './Classes/CompararServiços.php';
+<div class="bg-dark text-white position-absolute top-50 start-50 translate-middle">
+    <?php
+    ini_set('default_charset', 'UTF-8');
+    include_once './Classes/config.php';
+    require_once './Classes/ConexaoBD.php';
+    require_once './Classes/CompararServiços.php';
 
-$conexao = new ConexaoBD($nomeServidor, $nomeUsuario, $senha, $nomeBanco);
-$conexao->conectar();
+    $conexao = new ConexaoBD($nomeServidor, $nomeUsuario, $senha, $nomeBanco);
+    $conexao->conectar();
 
-$listaServicos = new CompararServicos($conexao);
-$servicos = $listaServicos->GetServicos();
+    $listaServicos = new CompararServicos($conexao);
+    $servicos = $listaServicos->GetServicos();
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['servico1']) && isset($_POST['servico2'])) {
-    $servico1Id = $_POST['servico1'];
-    $servico2Id = $_POST['servico2'];
+    if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['servico1']) && isset($_POST['servico2'])) {
+        $servico1Id = $_POST['servico1'];
+        $servico2Id = $_POST['servico2'];
 
-    $servico1Dados = $listaServicos->GetServicoPorId($servico1Id);
-    $servico2Dados = $listaServicos->GetServicoPorId($servico2Id);
+        $servico1Dados = $listaServicos->GetServicoPorId($servico1Id);
+        $servico2Dados = $listaServicos->GetServicoPorId($servico2Id);
 
-    if ($servico1Dados !== null && $servico2Dados !== null) {
-        echo "<h3 class='m-2'>Comparação de Serviços</h3>";
-        
-        echo "<div class='table-container'>";
-        echo "<table>";
-        echo "<tr><th>Serviço 1</th></tr>";
-        echo "<tr><td>";
-        $listaServicos->MostrarServicosSelecionados($servico1Dados);
-        echo "</td></tr>";
-        echo "</table>";
-        echo "</div>";
-        
-        echo "<div class='table-container'>";
-        echo "<table>";
-        echo "<tr><th>Serviço 2</th></tr>";
-        echo "<tr><td>";
-        $listaServicos->MostrarServicosSelecionados($servico2Dados);
-        echo "</td></tr>";
-        echo "</table>";
-        echo "</div>";
-        
-        echo "</div>";
+        if ($servico1Dados !== null && $servico2Dados !== null) {
+            echo "<h3 class='m-2'>Comparação de Serviços</h3>";
+
+            echo "<div class='row'>";
+            
+            echo "<div class='col-md-6'>";
+            echo "<div class='table-container'>";
+            echo "<table>";
+            echo "<tr><th>Serviço 1</th></tr>";
+            echo "<tr><td>";
+            $listaServicos->MostrarServicosSelecionados($servico1Dados);
+            echo "</td></tr>";
+            echo "</table>";
+            echo "</div>";
+            echo "</div>";
+
+            echo "<div class='col-md-6'>";
+            echo "<div class='table-container'>";
+            echo "<table>";
+            echo "<tr><th>Serviço 2</th></tr>";
+            echo "<tr><td>";
+            $listaServicos->MostrarServicosSelecionados($servico2Dados);
+            echo "</td></tr>";
+            echo "</table>";
+            echo "</div>";
+            echo "</div>";
+            
+            echo "</div>";
+        } else {
+            echo "Erro ao obter dados dos serviços selecionados.";
+        }
     } else {
-        echo "Erro ao obter dados dos serviços selecionados.";
-    }
-} else {
-    echo "<h3 class='m-2'>Selecione dois serviços para comparar:</h3>";
-    echo "<form action='' method='post'>";
-    echo "<div class='text-center'>";
-    echo "<label for='servico1'>Serviço 1:</label>";
-    echo "<select name='servico1' id='servico1'>";
-    foreach ($servicos as $servico) {
-        echo "<option value='{$servico['ID_SERVICO']}'>{$servico['SERVICO_DESCRICAO']}</option>";
-    }
-    echo "</select>";
+        echo "<h3 class='m-2'>Selecione dois serviços para comparar:</h3>";
+        echo "<form action='' method='post'>";
+        echo "<div class='text-center'>";
+        echo "<label for='servico1'>Serviço 1:</label>";
+        echo "<select name='servico1' id='servico1'>";
+        foreach ($servicos as $servico) {
+            echo "<option value='{$servico['ID_SERVICO']}'>{$servico['SERVICO_DESCRICAO']}</option>";
+        }
+        echo "</select>";
 
-    echo "<label for='servico2'>Serviço 2:</label>";
-    echo "<select name='servico2' id='servico2'>";
-    echo "</div>";
-    foreach ($servicos as $servico) {
-        echo "<option value='{$servico['ID_SERVICO']}'>{$servico['SERVICO_DESCRICAO']}</option>";
-    }
-    echo "</select>";
+        echo "<label for='servico2'>Serviço 2:</label>";
+        echo "<select name='servico2' id='servico2'>";
+        foreach ($servicos as $servico) {
+            echo "<option value='{$servico['ID_SERVICO']}'>{$servico['SERVICO_DESCRICAO']}</option>";
+        }
+        echo "</select>";
 
-    echo "<div class='mt-2 text-center'><button class='btn btn-light' type='submit'>Comparar</button></div>";
-    echo "</form>";
-}
-?>
-</form>
+        echo "<div class='mt-2 text-center'><button class='btn btn-light' type='submit'>Comparar</button></div>";
+        echo "</div>";
+        echo "</form>";
+    }
+    ?>
+</div>
 </body>
 
 </html>
